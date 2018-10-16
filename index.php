312f5f7d7d5c35e4cee95be902fdda9cce37ed73
Edgaras <eddygrandas@gmail.com>
2018-10-16 15:10:10 +0300

    vaikai

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <?php // nustatome laiko zoną
-  timezone_set('Europe/Vilnius');?>
     <head>
     <meta charset="utf-8">
-    <title>PHP lydės ir <?php print 'ryt '.date('D',strtotime('+1 day'));?>!</title>
+    <title>PHP lydės ir <?php print(date('Y', strtotime('+'.rand(1,10).' year')));?></title>
   </head>
   <body>
-      /! pamakalavom datą
-      <h1><?php print 'Edgaras' ?> - PHP su manim buvo ir <?php print date('H',strtotime('-1 hour')).' valandą!';?></h1>
      
-      <p><?php print date('Y', strtotime('+1 year'));?> ne už kalnų!</p>
+      <h1><?php print 'Edgaras' ?> - Galbūt turėsiu <?php print rand(1,5).' vaikų(us).';?></h1>
+     
+      <p>D. Trump'as nebebus prezidentu 
+          <?php print date('Y m d', strtotime('+'.rand(1,100).' year'));
+                 ?>
+              </p>
     
-   
-  </body>
+   </body>
 </html>
 
 
