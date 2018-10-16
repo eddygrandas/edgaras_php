386747ef3ca87a44ea30b025c4e6cbe1a0e62363
Edgaras <eddygrandas@gmail.com>
2018-10-16 13:29:31 +0300

    9

--- index.php
+++ index.php
@@ -2,12 +2,18 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Aš valdau HTML!</title>
+    <title>Aš valdau HTML <?php print 'ir PHP';?>!</title>
   </head>
   <body>
-  <?php 
-  print date('Y')
-  ?>  
+      
+      <h1><?php print 'Edgaras' ?> - HTML <?php print 'ir PHP ';?>asas</h1>
+      
+      <p>Jau moku kurti dinaminį turinį. Atidaryta: <?php 
+        print date("Y F jS H:i:s"); 
+      ?>
+      </p>
+    
+   
   </body>
 </html>
 
