e24d5aece29b2745989b6a99487d221f10512a46
Edgaras <eddygrandas@gmail.com>
2018-10-16 15:37:11 +0300

    rand

--- index.php
+++ index.php
@@ -4,11 +4,12 @@
     <meta charset="utf-8">
     <title>PHP lydės ir <?php print(date('Y m d', strtotime('+'.rand(1,10).' year')));?></title>
   </head>
-  <body>
+  <?php $spalva=rand(0,255); ?>
+  <body style='background-color:<?php printf( "#%06X\n", rand( 0, 0x222222 )); ?>'>
      
-      <h1><?php print 'Edgaras' ?> - Galbūt turėsiu <?php print rand(1,5).' vaikų(us).';?></h1>
+      <h1 style='font-size:<?php print(rand(16,200));?>'><?php print 'Edgaras' ?> - Galbūt turėsiu <?php print rand(1,5).' vaikų(us).';?></h1>
      
-      <p>D. Trump'as nebebus prezidentu 
+      <p style='color:<?php printf( "#%06X\n", rand( 0, 0x222222 )); ?>'>D. Trump'as nebebus prezidentu 
           <?php print date('Y m d', strtotime('+'.rand(1,100).' year'));
                  ?>
               </p>
