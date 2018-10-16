f915140059daa7a476a99c410147c5d8352a4c4f
Edgaras <eddygrandas@gmail.com>
2018-10-16 12:54:09 +0300

    sestas

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
-<?php
- print 'Veikia? taip Git bandau';
- ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Aš valdau HTML!</title>
+  </head>
+  <body>
+      
+      <h1>Edgaras - HTML asas</h1>
+      
+      <p>Jau moku kurti statinį turinį</p>
+    
+   
+  </body>
+</html>
 
 
