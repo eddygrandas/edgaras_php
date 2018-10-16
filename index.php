c031a0b74a8166d5fffe1b9f5857951a6b29a619
Edgaras <eddygrandas@gmail.com>
2018-10-16 14:26:36 +0300

    metai

--- index.php
+++ index.php
@@ -2,15 +2,13 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title><?php print 'Aš '.date('l').' ir PHP';?>!</title>
+    <title>PHP lydės ir <?php print 'ryt '.date('D',strtotime('+1 day'));?>!</title>
   </head>
   <body>
       
-      <h1><?php print 'Edgaras' ?> - HTML<?php print ' ir PHP ';?>asas jau nuo <?php print date('Y').' metų' ;?></h1>
+      <h1><?php print 'Edgaras' ?> - PHP su manim buvo ir <?php print date('H',strtotime('-1 hour')).' valandą!';?></h1>
      
-      <p>Viskas prasidėjo <?php print date('F').' mėnesio, '.date('d').' dieną'; 
-      ?>
-      </p>
+      <p><?php print date('Y', strtotime('+1 year'));?> ne už kalnų!</p>
     
    
   </body>
