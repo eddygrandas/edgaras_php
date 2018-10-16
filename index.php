fcda6533bf4242b932a1ebcc5f4b4ae363bdaf4b
Edgaras <eddygrandas@gmail.com>
2018-10-16 14:05:15 +0300

    data

--- index.php
+++ index.php
@@ -2,14 +2,13 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Aš valdau HTML <?php print 'ir PHP';?>!</title>
+    <title><?php print 'Aš '.date('l').' ir PHP';?>!</title>
   </head>
   <body>
       
-      <h1><?php print 'Edgaras' ?> - HTML <?php print 'ir PHP ';?>asas</h1><br>
-      <br>
-      <p>Jau moku kurti dinaminį turinį. Atidaryta: <?php 
-        print date('Y-m-d H:i:s'); 
+      <h1><?php print 'Edgaras' ?> - HTML<?php print ' ir PHP ';?>asas jau nuo <?php print date('Y').' metų' ;?></h1>
+     
+      <p>Viskas prasidėjo <?php print date('F').' mėnesio, '.date('d').' dieną'; 
       ?>
       </p>
     
