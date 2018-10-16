961b3f3d0ff120008795faabee53fc3dd5ad5c24
Edgaras <eddygrandas@gmail.com>
2018-10-16 14:35:12 +0300

    komentarai

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
+  <?php // nustatome laiko zoną
+  timezone_set('Europe/Vilnius');?>
+    <head>
     <meta charset="utf-8">
     <title>PHP lydės ir <?php print 'ryt '.date('D',strtotime('+1 day'));?>!</title>
   </head>
   <body>
-      
+      /! pamakalavom datą
       <h1><?php print 'Edgaras' ?> - PHP su manim buvo ir <?php print date('H',strtotime('-1 hour')).' valandą!';?></h1>
      
       <p><?php print date('Y', strtotime('+1 year'));?> ne už kalnų!</p>
